3ba8443eb69dcd7f10e7a2492156b914e6d7d43c
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-15 23:34:23 +0300

    expesnes chart

--- 1-components/src/components/Expenses/Expenses.js
+++ 1-components/src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 
-import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 import './Expenses.css'
 
 const Expenses = (props) => {
@@ -20,9 +21,8 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ))}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
