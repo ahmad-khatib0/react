b070224244c2d2661716213781612a4d1db6cd87
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-22 00:46:47 +0300

    YOU ADD AS DEPENDENCIES, WHAT YOU'RE USING IN YOUR SIDE EFFECT FUNCTION.

--- 4-side-effects/src/components/Login/Login.js
+++ 4-side-effects/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
@@ -11,16 +11,21 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState()
   const [formIsValid, setFormIsValid] = useState(false)
 
+  useEffect(() => {
+    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6)
+  }, [enteredEmail, enteredPassword])
+
+  // there is a simple rule, YOU ADD AS DEPENDENCIES, WHAT YOU'RE USING IN YOUR SIDE EFFECT FUNCTION.
+  // we omitted setFormIsValid here, which means it also could be added to the array
+  // So in this case, we are using, the setFormisValid function. We are using enteredEmail
+  // and we are using enteredPassword.
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value)
-
-    setFormIsValid(event.target.value.includes('@') && enteredPassword.trim().length > 6)
   }
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value)
-
-    setFormIsValid(event.target.value.trim().length > 6 && enteredEmail.includes('@'))
   }
 
   const validateEmailHandler = () => {
