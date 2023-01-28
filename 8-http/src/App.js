0cc4e6fe2fe0aa5a9bb83b7418b75ff972bdae24
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-28 17:55:44 +0300

    Using useEffect() For Requests

--- 8-http/src/App.js
+++ 8-http/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import MoviesList from './components/MoviesList'
 import './App.css'
@@ -8,14 +8,15 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     setError(null)
     try {
       const response = await fetch('https://swapi.dev/api/films/')
       if (!response.ok) {
-        throw new Error('Something went wrong')
+        throw new Error('Something went wrong!')
       }
+
       const data = await response.json()
 
       const transformedMovies = data.results.map((movieData) => {
@@ -27,19 +28,26 @@ function App() {
         }
       })
       setMovies(transformedMovies)
-    } catch (e) {
-      setError(e.message)
+    } catch (error) {
+      setError(error.message)
     }
     setIsLoading(false)
-  }
+  }, [])
 
-  let content = <p> found no movies</p>
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
+
+  let content = <p>Found no movies.</p>
+
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />
   }
+
   if (error) {
-    content = <p> {error} </p>
+    content = <p>{error}</p>
   }
+
   if (isLoading) {
     content = <p>Loading...</p>
   }
