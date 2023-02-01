7440517d6a552f991763ca1f679464b30710e153
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-02-01 11:38:00 +0300

    Managing The Overall Form Validity

--- 10-forms/src/components/BasicForm.js
+++ 10-forms/src/components/BasicForm.js
@@ -1,50 +1,19 @@
-import { useState } from 'react'
-
-const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('')
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
-
-  const enteredNameIsValid = enteredName.trim() !== ''
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
-
-  const nameInputChangeHandler = (event) => {
-    setEnteredName(event.target.value)
-  }
-
-  const nameInputBlurHandler = (event) => {
-    setEnteredNameTouched(true)
-  }
-
-  const formSubmissionHandler = (event) => {
-    event.preventDefault()
-
-    setEnteredNameTouched(true)
-
-    if (!enteredNameIsValid) {
-      return
-    }
-
-    console.log(enteredName)
-
-    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
-    setEnteredName('')
-    setEnteredNameTouched(false)
-  }
-
-  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
-
+const BasicForm = (props) => {
   return (
-    <form onSubmit={formSubmissionHandler}>
-      <div className={nameInputClasses}>
-        <label htmlFor="name">Your Name</label>
-        <input
-          type="text"
-          id="name"
-          onChange={nameInputChangeHandler}
-          onBlur={nameInputBlurHandler}
-          value={enteredName}
-        />
-        {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
+    <form>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
+        </div>
+      </div>
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
@@ -53,4 +22,4 @@ const SimpleInput = (props) => {
   )
 }
 
-export default SimpleInput
+export default BasicForm
