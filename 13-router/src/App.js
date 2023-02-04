eba2c58bf4c6b509e64e924d1c358c376e35f9a2
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-02-04 17:30:28 +0300

    Relative & Absolute Paths

--- 13-router/src/App.js
+++ 13-router/src/App.js
@@ -7,6 +7,7 @@ import {
 import ErrorPage from './pages/Error'
 
 import HomePage from './pages/Home'
+import ProductDetailPage from './pages/ProductDetail'
 import ProductsPage from './pages/Products'
 import RootLayout from './pages/Root'
 
@@ -24,8 +25,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> },
-      { path: '/products/:productId', element: <ProductDetailPage /> },
+      { path: 'products', element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> },
     ],
   },
 ])
