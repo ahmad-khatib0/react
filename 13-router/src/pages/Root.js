d4fe0924c6d2482ca8922161ead01689c987f909
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-02-04 16:57:52 +0300

    Showing Error Pages with errorElement

--- 13-router/src/pages/Root.js
+++ 13-router/src/pages/Root.js
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 import MainNavigation from '../components/MainNavigation'
-import classes from './Root.module.css'
 
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main>
         {/*  child routes will be rendered here  */}
         <Outlet />
       </main>
