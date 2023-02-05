6e2ba0d59fe2b84c75eecac58b2ebb31ce3c29bd
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-02-05 18:10:08 +0300

    add auth route

--- 15-authentication/frontend/src/components/MainNavigation.js
+++ 15-authentication/frontend/src/components/MainNavigation.js
@@ -23,6 +23,11 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          <li>
+            <NavLink to="/auth" className={({ isActive }) => (isActive ? classes.active : undefined)}>
+              Authentication
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <NewsletterSignup />
