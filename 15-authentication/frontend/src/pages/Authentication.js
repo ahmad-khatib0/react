9ea96ae5de7c4716d1d32a174b5400446d00cde2
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-02-05 19:57:07 +0300

    Managing the Token Expiratoin

--- 15-authentication/frontend/src/pages/Authentication.js
+++ 15-authentication/frontend/src/pages/Authentication.js
@@ -24,7 +24,9 @@ export async function action({ request }) {
 
   const response = await fetch('http://localhost:8080/' + mode, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(authData),
   })
 
@@ -40,6 +42,9 @@ export async function action({ request }) {
   const token = resData.token
 
   localStorage.setItem('token', token)
+  const expiration = new Date()
+  expiration.setHours(expiration.getHours() + 1)
+  localStorage.setItem('expiration', expiration.toISOString())
 
   return redirect('/')
 }
